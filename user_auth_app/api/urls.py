988684546b4ac9fdaf11c4c77e720055063fa347
Mario Winter <mariowinter.sg@gmail.com>
2025-01-12 20:16:04 +0100

    feat: add UserProfileBusinessList view for listing business profiles

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegistrationView, UserProfileDetail
+from .views import RegistrationView, UserProfileDetail, UserProfileBusniessList
 
 
 urlpatterns = [
     path('profile/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-detail'),
+    path('profile/business/', UserProfileBusniessList.as_view(), name='userprofile-business-list'),
     path('registration/', RegistrationView.as_view(), name='registration'),
     #path('login/', CustomLoginView.as_view(), name='login'),
 ]
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 from user_auth_app.models import UserProfile
 from user_auth_app.api.permissions import ProfilePermission
-from .serializers import RegistrationSerializer, UserProfileSerializer
+from .serializers import RegistrationSerializer, UserProfileSerializer, UserProfileBusinessSerializer
 
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
@@ -37,3 +37,8 @@ class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
         obj = UserProfile.objects.get(user_id=self.kwargs['pk'])
         self.check_object_permissions(self.request, obj)
         return obj
+    
+class UserProfileBusniessList(generics.ListAPIView):
+    queryset = UserProfile.objects.filter(type=UserProfile.UserType.BUSINESS)
+    serializer_class = UserProfileBusinessSerializer
+    permission_classes = [ProfilePermission]
