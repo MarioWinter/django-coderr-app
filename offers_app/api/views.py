9a85b6192d44a7cd9c211dfd56c33d09f88006ac
Mario Winter <mariowinter.sg@gmail.com>
2025-01-25 14:16:11 +0100

    feat(views): add OfferViewSet for managing offer-related endpoints

---
+++ offers_app/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets, generics
+from rest_framework.response import Response
+
+from .serializers import OfferSerializer, OfferDetailSerializer
+from offers_app.models import Offer, OfferDetail
+
+
+class OfferViewSet(viewsets.ModelViewSet):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
