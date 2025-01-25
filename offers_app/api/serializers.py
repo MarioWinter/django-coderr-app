c1f38241d15227dcd8b60186951ace1122108c88
Mario Winter <mariowinter.sg@gmail.com>
2025-01-25 13:39:07 +0100

    feat(serializers): add serializers for Offer and OfferDetail models

---
+++ offers_app/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from offers_app.models import Offer, OfferDetail
+
+class OfferDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OfferDetail
+        fields = ['id', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type']
+
+class OfferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Offer
+        fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'min_price', 'min_delivery_time', 'user_details']
+
+
