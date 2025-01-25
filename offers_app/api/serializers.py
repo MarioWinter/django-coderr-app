99d70fdcf11f3e2ca82b9a2a84ee2d9be41ba005
Mario Winter <mariowinter.sg@gmail.com>
2025-01-25 14:06:36 +0100

    feat(serializers): integrate UserSerializer in OfferSerializer for user details serialization

--- offers_app/api/serializers.py
+++ offers_app/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from offers_app.models import Offer, OfferDetail
+from user_auth_app.api.serializers import UserSerializer
 
 
 
@@ -9,7 +10,7 @@ class OfferDetailSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type']
 
 class OfferSerializer(serializers.ModelSerializer):
-    
+    user_details = UserSerializer(source='user', read_only=True)
     class Meta:
         model = Offer
         fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'min_price', 'min_delivery_time', 'user_details']
