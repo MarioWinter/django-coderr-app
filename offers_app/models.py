1ffa75a69691693222ba1051343431989f83bc17
Mario Winter <mariowinter.sg@gmail.com>
2025-01-25 13:24:56 +0100

    feat(models): add OfferDetail model with fields and constraints

--- offers_app/models.py
+++ offers_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator
 
 class Offer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="offers")
@@ -24,4 +24,26 @@ class Offer(models.Model):
     def min_delivery_time(self):
         return self.details.aggregate(models.Min('delivery_time_in_days'))['delivery_time_in_days__min']
 
+class OfferDetail(models.Model):
+    class OfferType(models.TextChoices):
+        BASIC = 'basic', 'basic'
+        STANDARD = 'standard', 'standard'
+        PREMIUM = 'premium', 'premium'
 
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE, related_name='details')
+    title = models.CharField(max_length=255)
+    revisions = models.IntegerField(validators=[MinValueValidator(-1)], default=-1)
+    delivery_time_in_days = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField()
+    offer_type = models.CharField(
+        max_length=10, 
+        choices=OfferType.choices, 
+        default=OfferType.BASIC
+        )
+    
+    class Meta:
+        unique_together = ('offer', 'offer_type')
+
+    def __str__(self):
+        return f"title: {self.title}, offer_type: ({self.offer_type})"
