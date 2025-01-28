50562177a1d7c7fe63a20e1ac8e11ccc7c4610fb
Mario Winter <mariowinter.sg@gmail.com>
2025-01-28 22:23:46 +0100

    feat(api): add permissions and routing for offers and offer details endpoints

--- offers_app/api/urls.py
+++ offers_app/api/urls.py
@@ -1,2 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from .views import OfferViewSet, OfferDetailViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'offers', OfferViewSet, basename='offers')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('/offerdetails/<int:pk>/', OfferDetailViewSet.as_view(), name='offerdetails'),
+]
 
--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -4,13 +4,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
 
 from .serializers import OfferSerializer, OfferDetailSerializer
-from .permissions import OfferDetailPermission
+from .permissions import OfferDetailPermission, OfferPermission
 from offers_app.models import Offer, OfferDetail
 
 
 class OfferViewSet(viewsets.ModelViewSet):
     queryset = Offer.objects.all().prefetch_related('details')
     serializer_class = OfferSerializer
+    permission_classes = [OfferPermission]
     filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
     filterset_fields = ['user', 'details__price', 'details__delivery_time_in_days']
     ordering_fields = ['updated_at', 'min_price']
