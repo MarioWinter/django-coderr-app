21836b9b34e0fae11b4e1b8028c67708a4460964
Mario Winter <mariowinter.sg@gmail.com>
2025-02-06 23:46:25 +0100

    feat(orders): add OrderSerializer and update Order model with offer_detail_id relation

--- orders_app/api/serializers.py
+++ orders_app/api/serializers.py
@@ -1,4 +1,27 @@
 from django.db import transaction, models
 from rest_framework import serializers
 
-   
+from orders_app.models import Order
+from offers_app.models import OfferDetail
+
+class OrderSerializer(serializers.ModelSerializer):
+    offer_detail_id = serializers.PrimaryKeyRelatedField(queryset=OfferDetail.objects.all(),source='offer_detail_id', write_only=True)
+    customer_user = serializers.PrimaryKeyRelatedField(read_only=True)
+    class Meta:
+        model = Order
+        fields = ['id', 'customer_user', 'business_user' 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type', 'status', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'customer_user', 'business_user' 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type', 'created_at', 'updated_at']
+        
+    def create(self, validated_data):
+        offer = validated_data.pop('offer_detail')
+        validated_data.update({
+            'title': offer.title,
+            'revisions': offer.revisions,
+            'delivery_time_in_days': offer.delivery_time_in_days,
+            'price': offer.price,
+            'features': offer.features,
+            'offer_type': offer.offer_type,
+            'busines_user': self.context['request'].user.id
+        })
+        return super().create(validated_data)
+    
--- orders_app/models.py
+++ orders_app/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from offers_app.models import OfferDetail
 
 class Order(models.Model):
     class OfferType(models.TextChoices):
         BASIC = 'basic', 'basic'
         STANDARD = 'standard', 'standard'
         PREMIUM = 'premium', 'premium'
-    customer_user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="orders")
+    offer_detail_id = models.ForeignKey(OfferDetail, on_delete=models.PROTECT, related_name="order_offer_detail", null=True)
+    customer_user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="customer_user")
     busines_user = models.PositiveIntegerField(validators=[MinValueValidator(1)], null=True, blank=True)
     title = models.CharField(max_length=255, blank=True, null=True)
     revisions = models.IntegerField(validators=[MinValueValidator(-1)], default=-1, blank=True, null=True)
     delivery_time_in_days = models.PositiveIntegerField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2,blank=True, null=True)
-    features = models.JSONField(default=[], blank=True, null=True)
+    features = models.JSONField(default=list, blank=True, null=True)
     offer_type = models.CharField(max_length=10, choices=OfferType.choices, default=OfferType.BASIC, blank=True, null=True)
     status = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
