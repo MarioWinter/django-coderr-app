643ccc9fef1199d1977f7371fbca565f20e4391c
Mario Winter <mariowinter.sg@gmail.com>
2025-02-07 22:42:24 +0100

    feat(orders): add CustomerPermission to ContactViewSet

---
+++ orders_app/api/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+class CustomerPermission(permissions.BasePermission):
+    """
+    Allows only users with the type "customer" to create new resources.
+    This permission only applies to POST requests; all other methods are allowed.
+    """
+    def has_permission(self, request, view):
+        if request.method != 'POST':
+            return True
+        
+        return bool(
+            request.user and 
+            request.user.is_authenticated and
+            request.user.user_type == 'customer'
+        )
--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
+from .permissions import CustomerPermission
 
 from orders_app.models import Order
 from .serializers import OrderSerializer
@@ -8,6 +9,7 @@ from .serializers import OrderSerializer
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [CustomerPermission]
     
     def perform_create(self, serializer):
         serializer.save(customer_user=self.request.user)
